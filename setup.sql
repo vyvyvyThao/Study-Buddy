832dfd78d1456ea68356b42dae1428967f0fe492
ago12627 <audrey12627@gmail.com>
2024-12-07 19:53:38 -0500

    add example study sets into test and test2 users

--- setup.sql
+++ setup.sql
@@ -106,3 +106,6 @@ VALUES
 (1, 1, 'Filler text for setup by test', '2024-11-12 18:57:25');
 
 INSERT INTO friendships(user1_id, user2_id, user1_accepted, user2_accepted) VALUES (1, 2, true, true), (1,4, true, true), (2,3, true, true), (2,4, true, true);
+
+INSERT INTO study_sets(id, creator_id, title) VALUES (1, 1, 'SE310'), (2, 1, 'French'), (3, 2, 'German');
+INSERT INTO flashcards(studyset_id, front, back) VALUES (1, 'process of breaking down a complex process into smaller, simpler parts', 'functional decomposition'), (1, 'object relies on another to provide a sepecified set of functionality', 'delegation'), (1, 'manner & degree of interdependence', 'coupling'), (1, 'degree to which elements of a module belong together', 'cohesion'), (2, 'Hello', 'Bonjour'), (2, 'Goodbye', 'Au revoir'), (2, 'Thank you', 'Merci'), (3, 'Good day', 'Guten tag'), (3, 'Thanks', 'Danke'), (3, 'Goodbye', 'Auf Wiedersehen');
