dcc7a84d60090d21a1afacbcf266c4e1d9b9d435
Mounixa <mounica@pressionllc.com>
2024-12-09 12:00:59 -0500

    nav bar studysets

--- app/public/study-sets/study-sets.js
+++ app/public/study-sets/study-sets.js
@@ -235,3 +235,27 @@ document.addEventListener("DOMContentLoaded", () => {
 
     fetchStudySets();
 });
+
+let studySetsBtn = document.getElementById("studysets");
+let friendsBtn = document.getElementById("friends");
+let logoutBtn = document.getElementById("logout");
+
+studySetsBtn.addEventListener("click", () => {
+    window.location.href = "/study-sets";
+});
+
+friendsBtn.addEventListener("click", () => {
+    window.location.href = "/friends";
+});
+
+logoutBtn.addEventListener("click", () => {
+  fetch("/logout", {
+    method: "POST",
+    credentials: "include",
+  }).then((response) => {
+    return response.json();
+  }).then((body) => {
+    window.location.href = "/";
+    // window.location = body.url;
+  })
+});
