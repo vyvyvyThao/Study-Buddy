49afbdf99327d8e7f4113b55764b1f10138849fe
ago12627 <audrey12627@gmail.com>
2024-12-09 13:39:23 -0500

    more consistent navbar styling

--- app/public/study-sets/study-sets.js
+++ app/public/study-sets/study-sets.js
@@ -236,26 +236,3 @@ document.addEventListener("DOMContentLoaded", () => {
     fetchStudySets();
 });
 
-let mypageBtn = document.getElementById("mypage");
-let friendsBtn = document.getElementById("friends");
-let logoutBtn = document.getElementById("logout");
-
-mypageBtn.addEventListener("click", () => {
-    window.location.href = "/my-page";
-});
-
-friendsBtn.addEventListener("click", () => {
-    window.location.href = "/friends";
-});
-
-logoutBtn.addEventListener("click", () => {
-  fetch("/logout", {
-    method: "POST",
-    credentials: "include",
-  }).then((response) => {
-    return response.json();
-  }).then((body) => {
-    window.location.href = "/";
-    // window.location = body.url;
-  })
-});
